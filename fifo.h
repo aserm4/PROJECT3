82400ae9739367ae51299ce271d88b9e612e9033
aserm4 <shabusch@mail.mvnu.edu>
2026-03-26 14:47:41 -0400

    project3v2

--- fifo.h
+++ fifo.h
@@ -1,30 +1,58 @@
+/***************************************************************************
+* fifo.h - code to allow interprocess communication via a fifo,
+* or "named pipe"
+* copyright : (C) 2009 by Jim Skon
+*
+* This code permits the creation and use of FIFOs for communication
+* between processes.  
+* 
+* Revised by Bob Kasper (2/12/2022):
+*     use special class directory instead of /tmp for improved web security
+* the named piped is created and used in /home/class/csc3004/tmp
+***************************************************************************/
 #ifndef FIFO_H
 #define FIFO_H
 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string.h>
 #include <string>
 using namespace std;
 
-// Max message size (must be > longest verse ~535 chars)
-const int MaxMess = 800;
+#define MaxMess 800
 
-// MUST be your username followed by "_"
+// PATH changed from /tmp to special class directory for improved web security
+const string PATH  = "/home/class/csc3004/tmp/";
+
+// SIGniture assures the pipe is unique amoung users
 const string SIG = "shabusch_";
+#define MODE 0777		// allow pipe to be read and written by any user
+#define MESSTERM '\n'
 
 class Fifo {
-public:
-    Fifo();                 // Default constructor
-    Fifo(string);           // Construct with pipe name
 
-    void openread();        // Open for reading
-    void openwrite();       // Open for writing
-    void fifoclose();       // Close pipe
+ private: 
+  int fd;   // File descriptor for pipes
+  string pipename;
 
-    string recv();          // Receive message
-    void send(string);      // Send message
+ public:
+  // create a named pipe (FIFO)
+  Fifo();
+  Fifo(string);
 
-private:
-    string pipename;        // Name of pipe
-    int fd;                 // File descriptor
+  void openread();    // Start a new read transaction
+  void openwrite();   // Start a new write transaction
+  void fifoclose();   // Finish a transaction
+
+  string recv();      // Get the next record (after openread)
+  void send(string);  // Send a record (after openwrite)
 };
-
 #endif
